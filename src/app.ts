e9fca8e12c1b9b513df4ec42e4e310bab8f893a3
Michael Szul <michael@szul.us>
2017-07-07 21:44:21 -0400

    Tutorial #6

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,21 @@
 /// <reference path="../node_modules/botbuilder/lib/botbuilder.d.ts" />
 
 import * as builder from "botbuilder";
+
+var conn = new builder.ConsoleConnector().listen();
+var bot = new builder.UniversalBot(conn);
+
+bot.dialog("/", [
+    (sess, args, next) => {
+        if(!sess.userData.name) {
+            builder.Prompts.text(sess, "Hello, user! What is your name?");
+        }
+        else {
+            next();
+        }
+    },
+    (sess, result) => {
+        sess.userData.name = result.response;
+        sess.send(`Hello, ${sess.userData.name}`);
+    }
+]);
