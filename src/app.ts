e66aa1eb48e0783d205c690f6f5ef7b3184cb947
Michael Szul <michael@szul.us>
2017-07-08 20:44:39 -0400

    Changed to chat connector

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
 /// <reference path="../node_modules/botbuilder/lib/botbuilder.d.ts" />
+/// <reference path="../node_modules/@types/restify/index.d.ts" />
 
 import * as builder from "botbuilder";
+import * as restify from "restify";
 
-var conn = new builder.ConsoleConnector().listen();
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log(`Listening... ${server.name}... ${server.url}`);
+});
+
+var conn = new builder.ChatConnector({
+      appId: process.env.MICROSOFT_APP_ID
+    , appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
 var bot = new builder.UniversalBot(conn);
+server.post("/api/messages", conn.listen());
 
 bot.dialog("/", [
     (sess, args, next) => {
