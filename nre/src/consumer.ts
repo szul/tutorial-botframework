c7a2568e3397edeb39fcccb8976e0fe436f6a86c
szul <michael@szul.us>
2018-01-14 11:30:01 -0500

    Azure storage inclusion and gunzip of data files from NRE

---
+++ nre/src/consumer.ts
@@ -0,0 +1,6 @@
+/// <reference path="../../node_modules/@types/cheerio/index.d.ts" />
+/// <reference path="../../node_modules/@types/node/index.d.ts" />
+/// <reference path="../../node_modules/azure-storage/typings/azure-storage/azure-storage.d.ts" />
+
+import * as azure from "azure-storage";
+
