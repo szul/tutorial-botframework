2dc332b12cb120a9801f886096b10dec2f464dfb
szul <michael@szul.us>
2018-02-03 09:38:17 -0500

    Data changes

--- nre/src/consumer.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../../node_modules/@types/cheerio/index.d.ts" />
-/// <reference path="../../node_modules/@types/node/index.d.ts" />
-/// <reference path="../../node_modules/azure-storage/typings/azure-storage/azure-storage.d.ts" />
-
-import * as azure from "azure-storage";
-
